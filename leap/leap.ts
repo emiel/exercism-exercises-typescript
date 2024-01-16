17cf76b153ef95a196b5488426422b1f11328de0
Emiel van de Laar <gemiel@gmail.com>
2024-01-16 23:18:35 +0100

    Exercise: Leap

---
+++ leap/leap.ts
@@ -0,0 +1,15 @@
+export const isLeap = (year: number) : boolean => {
+  if (year % 4 !== 0) {
+    return false;
+  }
+
+  if (year % 100 !== 0) {
+    return true;
+  }
+
+  if (year % 400 !== 0) {
+    return false;
+  }
+
+  return true;
+};
